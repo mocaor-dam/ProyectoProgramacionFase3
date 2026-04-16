c2d824d1fca2838c035c730930e92e8d79a97cd2
Moises <mocaor2007@gmail.com>
2026-04-16 12:17:28 +0200

    Creacion del metodo cargarConfiguracion

--- src/main/java/aventura/app/CargadorAventura.java
+++ src/main/java/aventura/app/CargadorAventura.java
@@ -2,7 +2,11 @@ package aventura.app;
 
 import com.google.gson.Gson;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Properties;
 
 public class CargadorAventura {
     private Gson gson;
@@ -10,6 +14,30 @@ public class CargadorAventura {
     private Path archivoAventura;
 
     public void cargarConfiguracion(){
+        Properties config = new Properties();
+        Path rutaConfig = Path.of("config.properties");
+
+
+        //Cargamos el archivo
+        try (BufferedReader reader = Files.newBufferedReader(rutaConfig)){
+            config.load(reader);
+            //Leemos los Strings usando las "llaves" del archivo properties
+            String dir = config.getProperty("juego.directorio.base");
+            String archivo = config.getProperty("juego.archivo.base");
+
+
+            //Convertimos los Strings a objetos Path
+            this.directorioBase = Path.of(dir);
+            //Usamos resolve para unir la carpeta con el archivo
+            this.archivoAventura = this.directorioBase.resolve(archivo);
+
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
 
     }
+
+
 }
